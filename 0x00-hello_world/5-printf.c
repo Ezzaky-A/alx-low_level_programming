4d66c1348040d2efe4266dc296ddc6eea25a6594
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-16 14:03:00 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - this prints hello
+  * Return: This returns 0
+  */
+int main(void)
+{
+print("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
