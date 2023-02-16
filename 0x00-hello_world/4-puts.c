10fd88440d1158a1096303b8f736506e713c2e9a
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-16 13:43:29 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-  int main(void) 
-      {
+
+/**
+  * main - this prints hello
+  * Return: This returns 0
+  */
+
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-      }   
+}   
