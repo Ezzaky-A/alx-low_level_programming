8cc69a029719cfd9efd17dc1570ca89950373486
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-19 14:50:42 -0800

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = 0; n1 <= 8; n1++)
+	{
+		for (n2 = 1; n2 <= 9; n2++)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				if (n2 < 89)
+				{
+					putchar(',');
+				}
+				putchar('0' + n1);
+				putchar('0' + n2);
+			}
+		}
+	}
+	return (0);
+}
