4f4f39220b2c8c388a61084668be72a65f2c7df6
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-20 07:24:11 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,17 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Two_digit seperate by comma'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char str[];
 
-int main(void) {
-    const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, sizeof(str) - 1);
-    return 1;
+	str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, sizeof(str) - 1);
+	return (1);
 }
-
