60cc95fbd74759bda000ef6584f69d2e4537601e
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-21 08:15:27 -0800

    task nta3 lhoma9

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alphabets
+ * Return: 1
+ */
+void print_alphabet_x10(void)
+{
+	int c, n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+	}
+
+
+}
