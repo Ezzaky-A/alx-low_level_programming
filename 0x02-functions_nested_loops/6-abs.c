fec1c3064c15d2f6ce16215b42e38ffaa4aae8aa
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-21 11:20:19 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs-print the absolute value of integer
+ * @int:checker
+ * Return:the absolute value;
+ */
+int _abs(int)
+{
+	int x;
+
+	if (x < 0)
+	{
+		x = x * (-1);
+	}
+}
