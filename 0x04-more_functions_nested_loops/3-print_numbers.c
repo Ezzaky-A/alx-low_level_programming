076354586275e6decd6c4433ee136c476bd80200
Ezzaky-A <aichaezzaky08@gmail.com>
2023-02-23 09:34:42 -0800

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  * print_numbers - checks for checks for a digit (0 through 9).
  *
- * Return: Always 0.
+ * Return: nothing.
  */
 void print_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 59; n++)
+	for (n = 48; n < 58; n++)
 	{
 		_putchar(n);
 	}
