2324725102a4323a1f28dce64653e4a07063345d
Ezzaky-A <aichaezzaky08@gmail.com>
2023-03-21 10:25:29 -0700

    task1

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog-function that prints a struct dog
+ * @d: variabl struct
+ * Return: nhothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d is NULL)
+	{
+		printf("");
+	}
+	else if (name is NULL)
+	{
+		printf("name: (nil)");
+	}
+}
