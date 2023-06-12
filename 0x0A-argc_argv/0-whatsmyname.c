c30a24e8334fcefc8e1b501a10304027a3015d17
Ezzaky-A <aichaezzaky08@gmail.com>
2023-06-12 05:49:57 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main-program that prints its name
+ * @argc:number of comnd line
+ * @argv:array of commande line
+ * Return:something
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
